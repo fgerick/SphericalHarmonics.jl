14b92e8a6946818aa4c205faebdfe7183957d44d
Felix Gerick <felixgerick@gmail.com>
2020-06-10 11:37:48 +0200

    typed ylm rlm

--- src/sphericalHarmonic.jl
+++ src/sphericalHarmonic.jl
@@ -1,28 +1,50 @@
 #normalization factor
-function ylmKCoefficient(l::Int64, m::Int64)
+abstract type YLMNorm end
 
-  k::Float64 = 1
+struct Schmidt{T} <: YLMNorm
+end
+
+struct Laplace{T} <: YLMNorm
+end
+
+struct Ylm{T}; end
+struct Rlm{T}; end
+struct Rlylm{T}; end
+
+function ylmKCoefficient(N::Schmidt{T},l::Int64, m::Int64) where T
+
+	k = one(T)
+	for i in (l-m+1):(l+m)
+	  k *= i
+	end
+	return sqrt(1/k)
+end
+
+function ylmKCoefficient(N::Laplace{T}, l::Int64, m::Int64) where T
+
+  k = one(T)
   for i in (l-m+1):(l+m)
     k *= i
   end
 
-  return sqrt((2*l+1) / (4*pi*k))
+  return sqrt((2*l+1) / (4*T(pi)*k))
 end
+
 
 function ylmCosSinPolynomial(m::Int64, x::Variable, y::Variable)
 
-  sum = 0
-  for j::Int64 in 0:floor(m/2)
-    sum += ((-1)^j)*binomial(m, 2*j)*(y^(2*j))*(x^(m-2*j))
+  sum = zero(x*y)
+  for j in 0:div(m,2)
+    sum += ((-1)^j)*binomial(m, 2j)*(y^(2j))*(x^(m-2j))
   end
   return sum
 end
 
 function ylmSinSinPolynomial(m::Int64, x::Variable, y::Variable)
 
-  sum = 0
-  for j::Int64 in 0:floor((m-1)/2)
-    sum += ((-1)^j)*binomial(m, 2*j + 1)*(y^(2*j + 1))*(x^(m-2*j-1))
+  sum = zero(x*y)
+  for j in 0:div((m-1),2)
+    sum += ((-1)^j)*binomial(m, 2j + 1)*(y^(2j + 1))*(x^(m-2j-1))
   end
   return sum
 end
@@ -37,7 +59,7 @@ end
 
 *Output:*  Spherical harmonic polynomial
 """
-function ylm(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable)
+function Ylm{T}(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable; norm::YLMNorm=Laplace{T}()) where T
 
   if abs(m) > l
     throw(DomainError(m,"-l <= m <= l expected, but m = $m and l = $l."))
@@ -46,22 +68,22 @@ function ylm(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable)
   p = (z^2 - 1)^l
 
   for i = 1:l+abs(m)
-    c = i <= l ? 1/(2*i) : 1.0
+    c = i <= l ? 1/(2one(T)*i) : one(T)
     p = c*differentiate(p, z)
   end
 
   if m > 0
-    return sqrt(2)*ylmKCoefficient(l, m)*ylmCosSinPolynomial(m,x,y)*p
+    return sqrt(2one(T))*ylmKCoefficient(norm, l, m)*ylmCosSinPolynomial(m,x,y)*p
   elseif m < 0
-    return sqrt(2)*ylmKCoefficient(l, abs(m))*ylmSinSinPolynomial(abs(m),x,y)*p
+    return sqrt(2one(T))*ylmKCoefficient(norm, l, abs(m))*ylmSinSinPolynomial(abs(m),x,y)*p
   else
-    return ylmKCoefficient(l, 0)*p
+    return ylmKCoefficient(norm, l, 0)*p
   end
 end
 
 # multiplying r^l*ylm(x,y,z)
-function rlylm(l::Int64, m::Int, x::Variable, y::Variable, z::Variable)
-	p = ylm(l,m,x,y,z)
+function Rlylm{T}(l::Int64, m::Int, x::Variable, y::Variable, z::Variable; kwargs...) where T
+	p = Ylm{T}(l,m,x,y,z;kwargs...)
 	tout = []
 	# Zerlegung des Polynoms in Terme:
 	for t in terms(p)
@@ -74,8 +96,10 @@ function rlylm(l::Int64, m::Int, x::Variable, y::Variable, z::Variable)
 end
 
 # solid harmonics
-function rlm(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable)
-	rlm = rlylm(l,m,x,y,z)
-	rlm = sqrt(4*pi/(2*l+1))*rlm
+function Rlm{T}(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable; norm::YLMNorm=Laplace{T}()) where T
+	rlm = Rlylm{T}(l,m,x,y,z; norm=norm)
+	rlm = sqrt(4one(T)*T(pi)/(2*l+1))*rlm
 	return rlm
 end
+
+rlm(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable; norm::YLMNorm=Laplace{Float64}()) = Rlm{Float64}(l::Int64, m::Int64, x::Variable, y::Variable, z::Variable; norm=norm)
